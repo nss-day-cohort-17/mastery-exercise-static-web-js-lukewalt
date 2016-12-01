75c838eb982b8e73ac6251c97915606a802f67d8
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-01 14:45:04 -0600

    created variables with captured DOM elements

---
+++ quiz.js
@@ -0,0 +1,18 @@
+// Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
+
+var treePerameters = {
+        hight: null,
+        character: null
+}
+
+var userHeight = document.getElementById('inputHeight').innerHTML;
+var userCharacter = document.getElementById('inputCharacter').innerHTML;
+var growButton = document.getElementById('grow').innerHTML;
+
+var tree = function(obj) {
+    treePerameters.height += userHeight;
+    treePerameters.character += userCharacter;
+}
+
+
+grow.addEventListener("click", tree);

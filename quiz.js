f68e872ebcf6d4b9d989db2137557b6a06c7deae
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-02 10:33:04 -0600

    changed name of parameters for arguments

--- quiz.js
+++ quiz.js
@@ -4,28 +4,28 @@ var userHeight = document.getElementById('inputHeight');
 var userCharacter = document.getElementById('inputCharacter');
 
 // Create a `tree` function that should build a pine tree out of a character in the Chrome dev tools console.
-var tree = function(num1, num2) {
-    num1 = userHeight.value;
-    num2 = userCharacter.value;
+var tree = function(hit, char) {
+    hit = userHeight.value;
+    char = userCharacter.value;
 
-    if (userHeight.value === '' || userCharacter.value === '') {
+    if (hit === '' || char === '') {
         alert("Both inputs must have a value");
-    } else if (isNaN(userHeight.value)) {
+    } else if (isNaN(hit)) {
         alert("First value must be a number");
     } else {
 
         var final = ''; //declared empty varibale to put my finished tree
 
-        for (var i = 0; i < num1; i++) {
+        for (var i = 0; i < hit; i++) {
 
             //declared var inside of the for loop so the tree can be built in conjunction with counter variable
             var row = '';
 
             // [i = 3] -> input'7' - [3] - 1 = 3 spaces repeated from start
-            row += ' '.repeat(num1 - i - 1);
+            row += ' '.repeat(hit - i - 1);
 
             // takes charater input , repeats it 2[7] + 1 , \n tells console to make a new line
-            row += num2.repeat(2*i + 1) + "\n";
+            row += char.repeat(2*i + 1) + "\n";
 
             //takes the tree built in prior two lines and sets it equal to a new variable
             final += row;

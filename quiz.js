3aa67bfa3ceb88033f2f433c2372599b89f5e4eb
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-01 18:23:58 -0600

    added function that created tree

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 
-
+//captured user inputs from the DOM
 var userHeight = document.getElementById('inputHeight');
 var userCharacter = document.getElementById('inputCharacter');
 
@@ -7,16 +7,34 @@ var userCharacter = document.getElementById('inputCharacter');
 var tree = function(num1, num2) {
     num1 = userHeight.value;
     num2 = userCharacter.value;
+
     if (userHeight.value === '' || userCharacter.value === '') {
         alert("Both inputs must have a value");
     } else if (isNaN(userHeight.value)) {
         alert("First value must be a number");
+    } else {
+
+        var final = ''; //declared empty varibale to put my finished tree
+
+        for (var i = 0; i < num1; i++) {
+
+            //declared var inside of the for loop so the tree can be built in conjunction with counter variable
+            var row = '';
+
+            // [i = 3] -> input'7' - [3] - 1 = 3 spaces repeated from start
+            row += ' '.repeat(num1 - i - 1);
+
+            // takes charater input , repeats it 2[7] + 1 , \n tells console to make a new line
+            row += num2.repeat(2*i + 1) + "\n";
+
+            //takes the tree built in prior two lines and sets it equal to a new variable
+            final += row;
+        }
+
+        //prints full tree in one console line
+        console.log(final)
     }
-    console.log(num1)
-    console.log(num2)
-
 }
-
 
 var growButton = document.getElementById('grow');
 
